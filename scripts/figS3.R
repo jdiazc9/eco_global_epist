84958a47c512d55d4dffa8ec70dc702ce4ff44af
juandc <juan.diazcolunga@gmail.com>
2022-05-23 15:13:53 -0400

    updated figS3: does our method identify the optimal community? if not, how far off is it?

--- scripts/figS3.R
+++ scripts/figS3.R
@@ -37,7 +37,7 @@ rsq_vs_nInSample <- function(n_in_sample, data = data) {
   # make sure all FEEs were fitted (enough points in-sample), otherwise return NA
   if (any(is.na(fits[, 'b']))) {
     
-    return(NA)
+    return(c(r_squared = NA, F_opt = NA))
     
   } else {
     
@@ -49,7 +49,20 @@ rsq_vs_nInSample <- function(n_in_sample, data = data) {
     po <- merge(predF, oos, by = 'community', suffixes = c('_pred', '_obs'))
     r_squared <- cor(po$fun_pred, po$fun_obs)^2
     
-    return(r_squared)
+    # predicted best community
+    best <- rbind(data.frame(community = predF$community,
+                             fun = predF$fun,
+                             source = 'pred'),
+                  data.frame(community = data$community,
+                             fun = data$fun,
+                             source = 'obs'))
+    best <- best[order(best$fun, decreasing = T), ]
+    
+    data <- rbind(data, oos)
+    F_opt <- data$fun[data$community == best$community[1]]
+    F_opt <- F_opt/max(data$fun)
+    
+    return(c(r_squared = r_squared, F_opt = F_opt))
     
   }
 
@@ -57,24 +70,41 @@ rsq_vs_nInSample <- function(n_in_sample, data = data) {
 
 n_insample <- sort(rep(seq(60,200, by = 20), 200))
 rsq <- rep(NA, length(n_insample))
+F_opt <- rep(NA, length(n_insample))
 for (i in 1:length(rsq)) {
   
   print(i)
-  rsq[i] <- rsq_vs_nInSample(n_insample[i], data = data)
+  run <- rsq_vs_nInSample(n_insample[i], data = data)
+  rsq[i] <- run['r_squared']
+  F_opt[i] <- run['F_opt']
   
 }
 
 # plot
 rsq <- data.frame(n_insample = n_insample,
-                  rsq = rsq)
+                  rsq = rsq,
+                  F_opt = F_opt)
 rsq <- do.call(data.frame,
-               aggregate(formula = rsq ~ n_insample,
+               aggregate(formula = cbind(rsq, F_opt) ~ n_insample,
                          data = rsq,
                          FUN = function(x) c(mean = mean(x, na.rm = T), err_minus = quantile(x, 0.05, na.rm = T), err_plus = quantile(x, 0.95, na.rm = T))))
 rsq$n_insample <- rsq$n_insample / 2^8
 
+rsq <- rbind(data.frame(n_insample = rsq$n_insample,
+                        metric = 'R_squared',
+                        mean = rsq$rsq.mean,
+                        err_minus = rsq$rsq.err_minus.5.,
+                        err_plus = rsq$rsq.err_plus.95.),
+             data.frame(n_insample = rsq$n_insample,
+                        metric = 'F_opt',
+                        mean = rsq$F_opt.mean,
+                        err_minus = rsq$F_opt.err_minus.5.,
+                        err_plus = rsq$F_opt.err_plus.95.))
+rsq$n_insample[rsq$metric == 'R_squared'] <- rsq$n_insample[rsq$metric == 'R_squared'] + 0.005
+rsq$n_insample[rsq$metric == 'F_opt'] <- rsq$n_insample[rsq$metric == 'F_opt'] - 0.005
+
 myplot <- 
-  ggplot(rsq, aes(x = n_insample, y = rsq.mean, ymax = rsq.err_minus.5., ymin = rsq.err_plus.95.)) +
+  ggplot(rsq, aes(x = n_insample, y = mean, ymin = err_minus, ymax = err_plus, color = metric)) +
     geom_errorbar(width = 0) +
     geom_line() +
     geom_point(cex = 3) +
@@ -83,7 +113,12 @@ myplot <-
     scale_y_continuous(name = expression(paste('Predicted vs. observed ',italic(R^2))),
                        limits = c(0, 1),
                        breaks = c(0, 0.5, 1),
-                       labels = c('0', '0.5', '1')) +
+                       labels = c('0', '0.5', '1'),
+                       sec.axis = sec_axis(trans=~.*1,
+                                           name = expression(paste(italic(F), ' ( ', bold(s)[opt], ' ) / ', italic(F)[max], sep = '')),
+                                           breaks = c(0, 0.5, 1),
+                                           labels = c('0', '0.5', '1'))) +
+    scale_color_manual(values = c('red', 'black')) +
     theme_bw() +
     theme(panel.grid = element_blank(),
           strip.background = element_blank(),
@@ -91,12 +126,18 @@ myplot <-
                                     size = 10),
           aspect.ratio = 0.6,
           axis.text = element_text(size = 16),
+          axis.text.y.right = element_text(size = 16,
+                                           color = 'red'),
           axis.title = element_text(size = 18),
+          axis.title.y.right = element_text(size = 18,
+                                            color = 'red'),
+          axis.ticks.y.right = element_line(color = 'red'),
           panel.border = element_blank(),
           panel.background = element_blank(),
           legend.position = 'none') +
     annotate("segment", x=-Inf, xend=Inf, y=-Inf, yend=-Inf, size=0.5) +
-    annotate("segment", x=-Inf, xend=-Inf, y=-Inf, yend=Inf,size=0.5)
+    annotate("segment", x=-Inf, xend=-Inf, y=-Inf, yend=Inf,size=0.5) +
+    annotate("segment", x=Inf, xend=Inf, y=-Inf, yend=Inf,size=0.5, color = 'red')
 
 print(myplot)
 ggsave(myplot,
